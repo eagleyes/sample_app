bd8cf7b8af92bce700dbff04b6d9ae4a59d4cffa
eagleyes <eagleyes12@gmail.com>
2010-11-21 20:06:51 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,1 @@
+This is a sample application for [*Ruby on Rails Tutorial: Learn Rails by Example*] (http://railstutorial.org/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '24be0000e7e9fdadbf9a60edf3bcd98c39ac314b80bff909935a165386d5f70a2a47141b4ab5ea1345d7ef6f850cb404be6305160f1328895b11a5018ea65ea5'
