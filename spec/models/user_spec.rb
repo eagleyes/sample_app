1541f55b720aa3355492b996933138bff4764422
Eagleyes <eagleyes12@gmail.com>
2010-11-26 10:17:09 -0500

    Finished Chapter 6 Modeling user part one

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,33 @@ describe User do
 		no_name_user.should_not be_valid
 	end
 
-	it "should require an eamil address" do
+	it "should require an email address" do
 		no_name_user = User.new(@attr.merge(:email => ""))
 		no_name_user.should_not be_valid
+	end
+
+	it "should accept valid email addresses" do
+		addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+		addresses.each do |address|
+			valid_email_user = User.new(@attr.merge(:email => address))
+			valid_email_user.should be_valid
+		end
+	end
+
+	it "should reject invalid email addresses" do
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+		addresses.each do |address|
+			invalid_email_user = User.new(@attr.merge(:email => address))
+			invalid_email_user.should_not be_valid
+		end
+	end
+
+	it "should reject duplicate email addresses" do
+
+		User.create!(@attr)
+		user_with_duplicate_email = User.new(@attr)
+		user_with_duplicate_email.should_not be_valid
+
 	end
 	
 end
