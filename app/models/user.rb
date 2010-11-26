57219db700aea2fa34ca0cf06190c05395a40019
Eagleyes <eagleyes12@gmail.com>
2010-11-26 16:19:45 -0500

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,16 @@ class User < ActiveRecord::Base
 												:confirmation => true,
 												:length => { :within => 6..40 }
 
+	before_save :encrypt_password
+
+	private
+
+		def encrypt_password
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			string
+		end
+	
 end
