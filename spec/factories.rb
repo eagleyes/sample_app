4961914016c0883fa9c2cb05a5cf70d407aaac1f
eagleyes <eagleyes12@gmail.com>
2010-12-01 14:47:52 -0500

    Finished Chapter 7, Modeling users

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name						"Guillaume Dupere"
+	user.email						"eagleyes12@gmail.com"
+	user.password					"password"
+	user.password_confirmation	"password"
+end
